3e9902940d453bcba215ccde2908dc7820142913
Octavio Deliberato Neto <octavio.deliberato@gmail.com>
2019-05-28 16:26:46 -0300

    Primeira coverage do Duf

--- pre_macro_generalities_duf.R
+++ pre_macro_generalities_duf.R
@@ -8,4 +8,29 @@ precos <- read_excel(filename, col_names = FALSE)
 
 ind <- str_which(precos$X__1, "Generalidades") + 1
 
-precos <- precos[ind:nrow(precos), c("X__1", "X__11")] %>% .[complete.cases(.), ]
+precos <- precos[ind:nrow(precos), c("X__1", "X__11")] %>% 
+  .[complete.cases(.), ]
+
+precos <- precos[c(1:9, 14, 15), ]
+
+de_para <- c("gris_value", "gris_min",
+             "toll_value",
+             "delivery_value",
+             "tas_value",
+             "other_fee_value", "other_fee_fraction", 
+             "cubic_factor",
+             "tda_fixed",
+             "trt_value", "trt_min",
+             "icms_tax_enable", "iss_tax_enable")
+
+lastRow <- precos[1:2, ]
+lastRow$X__1[1] <- "ICMS"; lastRow$X__11[1] <- "true"
+lastRow$X__1[2] <- "ISS"; lastRow$X__11[2] <- "true"
+precos <- rbind(precos, lastRow)
+precos$X__1 <- de_para
+
+ind <- which(precos$X__1 == "cubic_factor")
+precos$X__11[ind] %<>% str_extract("[:digit:]+")
+precos$X__11[1] <- as.numeric(precos$X__11[1]) * 100
+ind <- which(precos$X__1 == "trt_value")
+precos$X__11[ind] <- as.numeric(precos$X__11[ind]) * 100
