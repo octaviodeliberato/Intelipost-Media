4c1c4edf1c1ec570897f6e2df76168a6eddc3b48
Octavio Deliberato Neto <octavio.deliberato@gmail.com>
2019-05-31 13:59:11 -0300

    Primeira parte da freight table proveniente de pdf
    Com ajuda do pacote tabulizer, tive de delimitar a área desejada e selecionar a página do documento, pdf o que poderá comprometer o algoritmo em produção.

---
+++ pre_macro_freight_table_pdf.R
@@ -0,0 +1,32 @@
+library(tabulizer)
+library(dplyr)
+library(magrittr)
+library(stringr)
+library(DataExplorer)
+library(reshape2)
+
+# Location of pdf file
+location <- 'C:/Users/BlueShift075/Documents/55004_PROPOSTA - EB - 28.02.19.pdf'
+
+# Extract the table
+# locate_areas(location)
+out <- extract_tables(location, pages = 1, 
+                      output = "data.frame", area = list(c(190, 35,
+                                                           354, 395)))
+df <- out[[1]]
+names(df) <- df[1, ]
+df <- df[3:nrow(df), ]
+names(df) <- make.names(names(df))
+df <- update_columns(df, ind = 1:length(df), as.character)
+
+aux <- select_at(df, vars(contains("kg")))
+aux2 <- lapply(aux, str_extract, pattern = "...[:digit:],.+")
+aux2 <- lapply(aux2, str_replace_all, pattern = " ", replacement = "")
+aux2 <- as.data.frame(aux2)
+
+aux <- df[, !grepl("kg", colnames(df), ignore.case = T)]
+df <- cbind(aux, aux2)
+df[, grepl("Ad.", colnames(df), ignore.case = T)] %<>% str_replace("%", "")
+
+mynames <- names(df)[!grepl("kg", colnames(df), ignore.case = T)]
+df.m <- melt(df, id.vars = mynames)
